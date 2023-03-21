23e03b430e66ad8f6dbc20ab05b3d95bd3c23820
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-03-21 18:20:39 +0100

    Add get-balance endpoint

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const cors = require('cors')
+const tbtcv2bitcoin = require("@keep-network/tbtc-v2.ts/dist/src/bitcoin")
+const tbtcv2electrum = require("@keep-network/tbtc-v2.ts/dist/src/electrum")
 
 const app = express()
 app.use(cors())
 
+const privateKeyWif = "cRJvyxtoggjAm9A94cB86hZ7Y62z2ei5VNJHLksFi2xdnz1GJ6xt"
 const port = Number(process.env.PORT || "8080")
+const electrumUrl = process.env.ELECTRUM_URL || "wss://electrumx-server.test.tbtc.network:8443"
+const bitcoinClient = tbtcv2electrum.Client.fromUrl(electrumUrl)
 
-app.get('/get-balance/:addrOrPrivKey', async (req, res) => {
-    // TODO: Get balance
-    const addrOrPrivKey = req.params.addrOrPrivKey
+app.get('/get-balance', async (req, res) => {
+    const keyRing = tbtcv2bitcoin.createKeyRing(privateKeyWif)
+    const address = keyRing.getAddress("string")
+
+    const utxos = await bitcoinClient.findAllUnspentTransactionOutputs(
+        address
+    )
+
+    let balance = 0
+
+    for (let i = 0; i < utxos.length; i++) {
+        balance += utxos[i].value.toNumber()
+    }
 
     res.json({
-        balance: 0,
-        addrOrPrivKey: addrOrPrivKey
+        balance,
+        address
     })
 })
 
