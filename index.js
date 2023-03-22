1eb8797dec13e26b9376a5e601694462c8757b08
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2023-03-22 07:56:26 +0100

    Add send-transaction

--- index.js
+++ index.js
@@ -34,6 +34,73 @@ app.get('/get-balance', async (req, res) => {
     })
 })
 
+app.get('/send-transaction/:receiver/:amount', async (req, res) => {
+    const keyRing = tbtcv2bitcoin.createKeyRing(privateKeyWif)
+    const sender = keyRing.getAddress("string")
+
+    const receiver = req.params.receiver
+    const amount = Number(req.params.amount)
+
+    const utxos = await bitcoinClient.findAllUnspentTransactionOutputs(
+        sender
+    )
+
+    let inputSum = 0
+    const utxosWithRaw= []
+    for (const utxo of utxos) {
+        if (inputSum > amount) {
+            break
+        }
+
+        const utxoRawTransaction = await bitcoinClient.getRawTransaction(
+            utxo.transactionHash
+        )
+
+        utxosWithRaw.push({
+            ...utxo,
+            transactionHex: utxoRawTransaction.transactionHex,
+        })
+
+        inputSum += utxo.value.toNumber()
+    }
+
+    const inputCoins = utxosWithRaw.map((utxo) =>
+        bcoin.Coin.fromTX(
+            bcoin.MTX.fromRaw(utxo.transactionHex, "hex"),
+            utxo.outputIndex,
+            -1
+        )
+    )
+
+    const transaction = new bcoin.MTX()
+
+    transaction.addOutput({
+        script: bcoin.Script.fromAddress(receiver),
+        value: amount,
+    })
+
+    await transaction.fund(inputCoins, {
+        rate: null, // set null explicitly to always use the default value
+        changeAddress: sender,
+        subtractFee: false, // do not subtract the fee from outputs
+    })
+
+    transaction.sign(keyRing)
+
+    const transactionHash = tbtcv2bitcoin.TransactionHash.from(transaction.txid())
+
+    const rawTransaction = {
+        transactionHex: transaction.toRaw().toString("hex"),
+    }
+
+    await bitcoinClient.broadcast(rawTransaction)
+
+    res.json({
+        transactionHash: transactionHash.toString(),
+        transactionHex: rawTransaction.transactionHex
+    })
+})
+
 app.get('*', (_, res) => {
     res.set('Content-Type', 'text/html');
     const content = "<p>Specify exact route</p>"
